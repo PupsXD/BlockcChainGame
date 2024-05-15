816aaea00feff348886a21d2cf3de82b34570aa9
Petr Sokolov <74403997+PupsXD@users.noreply.github.com>
2024-05-16 01:07:48 +0300

    InitialCommit

---
+++ Blockchain test/Assets/Scripts/Player/AttackScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AttackScript : MonoBehaviour
+{
+    private Animator _animator;
+    
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }   
+
+    public void OnAttack(InputAction.CallbackContext value)
+    {
+        if (value.started)
+        {
+            _animator.SetTrigger("attack");
+            
+        }
+    }
+}
---
+++ Blockchain test/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class PlayerMovement : MonoBehaviour
+{
+   [SerializeField] private float _speed;
+   
+   private Vector2 _userInput;
+   
+   private Rigidbody2D _rb;
+   
+   private Animator _animator;
+
+   private bool _flipped = false;
+   
+   private void Awake()
+   {
+      _rb = GetComponent<Rigidbody2D>();
+      _animator = GetComponent<Animator>();
+   }
+
+
+   private void FixedUpdate()
+   {
+    Move();  
+   }
+   
+   public void OnMove(InputAction.CallbackContext value)
+   {
+      _userInput = value.ReadValue<Vector2>();
+      _animator.SetFloat("speed", _userInput.magnitude);
+      if (_userInput.x < 0 && !_flipped)
+      {
+         _flipped = true;
+      }
+      else if (_userInput.x > 0 && _flipped)
+      {
+         _flipped = false;
+      }
+      transform.localScale = new Vector3(_flipped ? -1 : 1, 1, 1);
+   }
+
+   private void Move()
+   {
+      _rb.MovePosition(_rb.position + _userInput * _speed * Time.fixedDeltaTime); 
+   }
+   
+   
+}
